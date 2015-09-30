631836ce68a4044dbb6483e23855def31e952447
YWJamesLin <YWJamesLin@gmail.com>
2015-09-30 17:19:52 +0800

    Init Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+usage :
+``` sh
+  curl -L https://raw.githubusercontent.com/YWJamesLin/zsh_env_generate/master/UserInit.sh | bash
+```
+  This will generate a normal user zsh environment.
+
+This script is to generate a basic oh-my-zsh based environment.
---
+++ UserInit.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+# User Environment Initialization
+# 2015.09.30 Created by YWJamesLin
+
+# Environment Variables
+#   ENV : 0 for normal users, 1 for root
+ENV="0"
+
+# ViM
+git clone git://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_basic_vimrc.sh
+sed -i 's/set cmdheight=/"set cmdheight=/' ~/.vimrc
+sed -i 's/set foldcolumn=/"set foldcolumn=/' ~/.vimrc 
+sed -i '142i " Show line numbers' ~/.vimrc
+sed -i '143i set nu' ~/.vimrc
+sed -i 's/colorscheme desert/ colorscheme default/' ~/.vimrc
+sed -i 's/set shiftwidth.*$/set shiftwidth=2/' ~/.vimrc
+sed -i 's/set tabstop.*$/set shiftwidth=2/' ~/.vimrc
+
+
+# zsh
+curl -L http://install.ohmyz.sh | sh
+git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+sed -i 's/ZSH_THEME=.*$/ZSH_THEME="YWJLTheme"/' ~/.zshrc
+sed -i 's/plugins=.*$/plugins=(git zsh-syntax-highlighting)/' ~/.zshrc
+git clone git://github.com/YWJamesLin/YWJLTheme.git ~/YWJLTheme
+cp ~/YWJLTheme/YWJLTheme.zsh-theme ~/.oh-my-zsh/themes/
+
+case ${ENV} in
+  "0")
+  curl -L https://raw.githubusercontent.com/YWJamesLin/zsh_env_generate/master/common/zsh/zshrc_user >> ~/.zshrc
+  ;;
+  "1")
+  curl -L https://raw.githubusercontent.com/YWJamesLin/zsh_env_generate/master/common/zsh/zshrc_root >> ~/.zshrc
+  ;;
+esac
+
+# Done
+echo "UserInit Complete."
