dd9a1f953f9193dc4a9d192f883f4365f22acc8a
YWJamesLin <JamesLin@ssinrc.org>
2015-11-04 11:33:02 +0800

    Separate ViM config

--- UserInit.sh
+++ UserInit.sh
@@ -6,17 +6,21 @@
 #   ENV : 0 for normal users, 1 for root
 ENV="0"
 
-# ViM
-git clone git://github.com/amix/vimrc.git ~/.vim_runtime
-sh ~/.vim_runtime/install_basic_vimrc.sh
-sed -i 's/set cmdheight=/"set cmdheight=/' ~/.vimrc
-sed -i 's/set foldcolumn=/"set foldcolumn=/' ~/.vimrc 
-sed -i '142i " Show line numbers' ~/.vimrc
-sed -i '143i set nu' ~/.vimrc
-sed -i 's/colorscheme desert/ colorscheme default/' ~/.vimrc
-sed -i 's/set shiftwidth.*$/set shiftwidth=2/' ~/.vimrc
-sed -i 's/set tabstop.*$/set shiftwidth=2/' ~/.vimrc
+#   We also provide ViM Profile to generate, 1 to generate , or not to with 0
+Generate_ViM="1"
 
+# ViM
+if [ ${Generate_ViM} == "1" ]; then
+  git clone git://github.com/amix/vimrc.git ~/.vim_runtime
+  sh ~/.vim_runtime/install_basic_vimrc.sh
+  sed -i 's/set cmdheight=/"set cmdheight=/' ~/.vimrc
+  sed -i 's/set foldcolumn=/"set foldcolumn=/' ~/.vimrc 
+  sed -i '142i " Show line numbers' ~/.vimrc
+  sed -i '143i set nu' ~/.vimrc
+  sed -i 's/colorscheme desert/ colorscheme default/' ~/.vimrc
+  sed -i 's/set shiftwidth.*$/set shiftwidth=2/' ~/.vimrc
+  sed -i 's/set tabstop.*$/set shiftwidth=2/' ~/.vimrc
+fi
 
 # zsh
 curl -L http://install.ohmyz.sh | sh
@@ -25,6 +29,7 @@ sed -i 's/ZSH_THEME=.*$/ZSH_THEME="YWJLTheme"/' ~/.zshrc
 sed -i 's/plugins=.*$/plugins=(git zsh-syntax-highlighting)/' ~/.zshrc
 git clone git://github.com/YWJamesLin/YWJLTheme.git ~/YWJLTheme
 cp ~/YWJLTheme/YWJLTheme.zsh-theme ~/.oh-my-zsh/themes/
+rm -rf ~/YWJLTheme
 
 case ${ENV} in
   "0")
