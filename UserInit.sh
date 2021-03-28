2c8aa4228735d093c17887bbf4f7c58d791208a9
YWJamesLin <ywjameslin@gmail.com>
2021-03-28 12:47:29 +0800

    [UserInit] Update sed syntax to fit MacOS sed behavior

--- UserInit.sh
+++ UserInit.sh
@@ -13,20 +13,20 @@ Generate_ViM="1"
 if [ ${Generate_ViM} == "1" ]; then
   git clone git://github.com/amix/vimrc.git ~/.vim_runtime
   sh ~/.vim_runtime/install_basic_vimrc.sh
-  sed -i 's/set cmdheight=/"set cmdheight=/' ~/.vimrc
-  sed -i 's/set foldcolumn=/"set foldcolumn=/' ~/.vimrc 
-  sed -i '142i " Show line numbers' ~/.vimrc
-  sed -i '143i set nu' ~/.vimrc
-  sed -i 's/colorscheme desert/ colorscheme default/' ~/.vimrc
-  sed -i 's/set shiftwidth.*$/set shiftwidth=2/' ~/.vimrc
-  sed -i 's/set tabstop.*$/set shiftwidth=2/' ~/.vimrc
+  sed -i '' 's/set cmdheight=/"set cmdheight=/' ~/.vimrc
+  sed -i '' 's/set foldcolumn=/"set foldcolumn=/' ~/.vimrc
+  sed -i '' '142i " Show line numbers' ~/.vimrc
+  sed -i '' '143i set nu' ~/.vimrc
+  sed -i '' 's/colorscheme desert/ colorscheme default/' ~/.vimrc
+  sed -i '' 's/set shiftwidth.*$/set shiftwidth=2/' ~/.vimrc
+  sed -i '' 's/set tabstop.*$/set shiftwidth=2/' ~/.vimrc
 fi
 
 # zsh
 curl -L http://install.ohmyz.sh | sh
 git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-sed -i 's/ZSH_THEME=.*$/ZSH_THEME="YWJLTheme"/' ~/.zshrc
-sed -i 's/plugins=.*$/plugins=(git zsh-syntax-highlighting)/' ~/.zshrc
+sed '' -i 's/ZSH_THEME=.*$/ZSH_THEME="YWJLTheme"/' ~/.zshrc
+sed '' -i 's/plugins=.*$/plugins=(git zsh-syntax-highlighting)/' ~/.zshrc
 git clone git://github.com/YWJamesLin/YWJLTheme.git ~/YWJLTheme
 cp ~/YWJLTheme/YWJLTheme.zsh-theme ~/.oh-my-zsh/themes/
 rm -rf ~/YWJLTheme
